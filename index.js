00d0761b339bddb7165032558a42a7b95f291089
Ben Bowles <benjamin.d.bowles@gmail.com>
2022-08-21 18:12:54 -0500

    fetch data from excel

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+const XLSX = require('xlsx');
 const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+const wb = XLSX.readFile(path.join(__dirname, './example/data.xlsx'));
+const xlData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {header: 1});
+
+console.log(xlData);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get(`/quiz`, (req, res) => {
-    res.render('main', {
-        title: 'Hello World',
-        message: [1, 2, 3, 4]})
+    res.send(XLSX.utils.sheet_to_html(wb.Sheets[wb.SheetNames[0]]))
 })
 
 app.listen(PORT, () => {
