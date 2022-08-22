efead0b74e567102a8803991637c3910ece748d4
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-08-22 15:07:09 -0500

    some stuff but I am tired so I am taking a break

--- public/js/elements.js
+++ public/js/elements.js
@@ -1,9 +1,19 @@
-customElements.define('answer-choice', class extends HTMLParagraphElement {
+customElements.define('answer-choice', class extends HTMLElement {
     
 });
-customElements.define('answer-container', class extends HTMLDivElement {
+customElements.define('answer-container', class extends HTMLElement {
 
 });
-customElements.define('question-choice', class extends HTMLDivElement {
+customElements.define('question-choice', class extends HTMLElement {
         
+});
+
+customElements.define('hint-button', class extends HTMLElement {
+
+});
+customElements.define('hint-text', class extends HTMLElement {
+
+});
+customElements.define('next-button', class extends HTMLElement {
+
 });
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,14 +1,18 @@
 const root = document.getElementById("root");
 
-const choice_A = document.getElementById("Choice_A");
-const choice_B = document.getElementById("Choice_B");
-const choice_C = document.getElementById("Choice_C");
-const choice_D = document.getElementById("Choice_D");
+const choices = [];
+
+choices.push(document.getElementById("Choice_A"));
+choices.push(document.getElementById("Choice_B"));
+choices.push(document.getElementById("Choice_C"));
+choices.push(document.getElementById("Choice_D"));
 
 const Question = document.getElementById("Question");
 
 const sheet_data = JSON.parse(document.getElementById("sheetData").innerText);
 sheet_data.shift();
+
+const hint_text = document.getElementById("Hint");
 
 var currentQuestion = 0;
 
@@ -17,8 +21,96 @@ root.onload = loadQuestion();
 function loadQuestion() {
     Question.innerText = sheet_data[currentQuestion]["Question"];
 
-    choice_A.innerText = sheet_data[currentQuestion]["A"];
-    choice_B.innerText = sheet_data[currentQuestion]["B"];
-    choice_C.innerText = sheet_data[currentQuestion]["C"];
-    choice_D.innerText = sheet_data[currentQuestion]["D"];
+    choices[0].innerText = sheet_data[currentQuestion]["A"];
+    choices[0].setAttribute("correct", sheet_data[currentQuestion]["Correct"] - 1 == 0);
+
+    choices[1].innerText = sheet_data[currentQuestion]["B"];
+    choices[1].setAttribute("correct", sheet_data[currentQuestion]["Correct"] - 1 == 1);
+
+    choices[2].innerText = sheet_data[currentQuestion]["C"];
+    choices[2].setAttribute("correct", sheet_data[currentQuestion]["Correct"] - 1 == 2);
+
+    choices[3].innerText = sheet_data[currentQuestion]["D"];
+    choices[3].setAttribute("correct", sheet_data[currentQuestion]["Correct"] - 1 == 3);
+
+    hint_text.innerText = sheet_data[currentQuestion]["Hint"];
+
+}
+
+function nextQuestion() {
+    if (currentQuestion >= sheet_data.length - 1) {
+        return console.log(getResults());
+    }
+    else
+    {
+        currentQuestion++;
+    }
+    loadQuestion();
+    updateQuestion();
+
+}
+
+function prevQuestion() {
+    
+    if (currentQuestion <= 0) {
+        return console.log("First Question");
+    }
+    else
+    {
+        currentQuestion--;
+    }
+    loadQuestion();
+    updateQuestion();
+
+}
+
+function submitAnswer(event) {
+    console.log(event.target.getAttribute('correct'));
+    
+    if (event.target.getAttribute('correct') == "true" && !sheet_data[currentQuestion]["Answered"]) {
+        sheet_data[currentQuestion]["Answered"] = true;
+        sheet_data[currentQuestion]["Selection"] = event.target;
+    }
+    else if (!sheet_data[currentQuestion]["Answered"])
+    {
+        sheet_data[currentQuestion]["Answered"] = true;
+        sheet_data[currentQuestion]["Selection"] = event.target;
+    }
+
+    updateQuestion();
+
+    console.log(sheet_data);
+}
+
+function updateQuestion() {
+    choices.forEach(choice => {
+        choice.classList.remove("correct");
+        choice.classList.remove("incorrect");
+    });
+    if (sheet_data[currentQuestion]["Answered"]) {
+        if (sheet_data[currentQuestion]["Selection"].getAttribute("correct") == "true") {
+            sheet_data[currentQuestion]["Selection"].classList.add("correct");
+        }
+        else {
+            sheet_data[currentQuestion]["Selection"].classList.add("incorrect");
+            //show correct answer
+            choices.forEach(choice => {
+                if (choice.getAttribute("correct") == "true") {
+                    choice.classList.add("correct");
+                }
+            });
+        }
+    }
+}
+
+function getResults() {
+    score = 0;
+    sheet_data.forEach(question => {
+        if (question["Answered"]) {
+            if (question["Selection"].getAttribute("correct") == "true") {
+                score++;
+            }
+        }
+    });
+    return `You got ${score} out of ${sheet_data.length} questions correct`;
 }
