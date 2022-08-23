00bac69743ce791cc100aede9904f5e321560fe1
Ben Bowles <benjamin.d.bowles@gmail.com>
2022-08-22 20:28:00 -0500

    all functionality working except hint styling

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -8,6 +8,7 @@ const HINT_TOGGLE = document.getElementById("HINT_TOGGLE");
 const HINT_TEXT = document.getElementById("HINT_TEXT");
 const NEXT_BUTTON = document.getElementById("NEXT");
 const PREV_BUTTON = document.getElementById("PREV");
+const SCORE_TEXT = document.getElementById('SCORE_TEXT');
 
 // data
 SHEET_DATA = JSON.parse(document.getElementById("SHEET_DATA").innerText); SHEET_DATA.shift();
@@ -18,6 +19,8 @@ CURRENT_QUESTION = 0;
 /* Functions */
 
 function loadQuestionDOM() {
+    updateScore();
+
     QUESTION.innerText = SHEET_DATA[CURRENT_QUESTION]["Question"];
     for (let i = 0; i < CHOICES.length; i++) {
         CHOICES[i].innerText = SHEET_DATA[CURRENT_QUESTION][i + 1];
@@ -25,6 +28,8 @@ function loadQuestionDOM() {
     CHOICES[SHEET_DATA[CURRENT_QUESTION]["Correct"] - 1].setAttribute('correct', true);
 
     HINT_TEXT.innerText = SHEET_DATA[CURRENT_QUESTION]["Hint"];
+
+    
 }
 
 function NEXT() {
@@ -44,21 +49,50 @@ function PREV() {
     }
 }
 
+function toggleHint() {
+    HINT_TEXT.classList.toggle('hidden');
+}
+
 function Answer(e) {
     if (!SHEET_DATA[CURRENT_QUESTION]["Answered"]) {
         SHEET_DATA[CURRENT_QUESTION]["Selected Answer"] = e.target.id;
         SHEET_DATA[CURRENT_QUESTION]["Answered"] = true;
-        console.log(e.target.id);
         updateQuestion();
     }
 }
 
 function updateQuestion() {
     if (SHEET_DATA[CURRENT_QUESTION]["Answered"]) {
-        if (SHEET_DATA[CURRENT_QUESTION]["Selected Answer"] === SHEET_DATA[CURRENT_QUESTION]["Correct"]) {
-            CHOICES[SHEET_DATA[CURRENT_QUESTION]["Selected Answer"]].innerText = "Correct";
+        if (parseInt(SHEET_DATA[CURRENT_QUESTION]["Selected Answer"]) === SHEET_DATA[CURRENT_QUESTION]["Correct"]) {
+            CHOICES[SHEET_DATA[CURRENT_QUESTION]["Selected Answer"] - 1].innerText = "Correct";
         } else {
-            console.log(2)
+            CHOICES[SHEET_DATA[CURRENT_QUESTION]["Selected Answer"] - 1].innerText = "Incorrect";
+            CHOICES[SHEET_DATA[CURRENT_QUESTION]["Correct"] - 1].innerText = "Correct";
         }
     }
+
+    updateScore();
+}
+
+function updateScore() {
+    SCORE_TEXT.innerText = `Score: ${getScore()}/${SHEET_DATA.length} \n ${parseInt(getScore()/SHEET_DATA.length*100)}%`;
+}
+
+function getScore() {
+    let score = 0;
+    for (let i = 0; i < SHEET_DATA.length; i++) {
+        if (SHEET_DATA[i]["Answered"]) {
+            if (parseInt(SHEET_DATA[i]["Selected Answer"]) === SHEET_DATA[i]["Correct"]) {
+                score++;
+            }
+        }
+    }
+    return score;
+}
+
+function reset() {
+    SHEET_DATA = JSON.parse(document.getElementById("SHEET_DATA").innerText); SHEET_DATA.shift();
+    CURRENT_QUESTION = 0;
+    loadQuestionDOM();
+    updateQuestion();
 }
