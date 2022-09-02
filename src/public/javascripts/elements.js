27b1090f6f9d5fb12305bc110b69b59c50216e6b
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-09-02 13:29:48 -0500

    custom elements

--- src/public/javascripts/elements.js
+++ src/public/javascripts/elements.js
@@ -1,13 +1,43 @@
-const custom_element_directives = [
-    customElements.define('c-question', class extends HTMLElement { }),
-    customElements.define('c-choices', class extends HTMLElement { }),
-    customElements.define('c-answer', class extends HTMLElement { }),
-    customElements.define('c-hint', class extends HTMLElement { }),
-    customElements.define('c-hint_text', class extends HTMLElement { }),
-    customElements.define('c-hint_toggle', class extends HTMLElement { }),
-    customElements.define('c-control', class extends HTMLElement { }),
-    customElements.define('c-next', class extends HTMLElement { }),
-    customElements.define('c-prev', class extends HTMLElement { }),
-    customElements.define('c-score_text', class extends HTMLElement { }),
-    customElements.define('c-reset', class extends HTMLElement { })
-];
+// quiz navigation custom elements
+class QuizNav extends HTMLElement {
+    constructor () {
+        super();
+
+        this.classList.add('material-symbols-outlined');
+    }
+}
+
+class Next extends QuizNav {
+    constructor () {
+        super();
+
+        this.innerHTML = 'navigate_next';
+    }
+}
+
+class Back extends QuizNav {
+    constructor () {
+        super();
+
+        this.innerHTML = 'navigate_before';
+    }
+}
+class Restart extends QuizNav {
+    constructor () {
+        super();
+
+        this.innerHTML = 'replay';
+    }
+}
+
+// quiz question custom elements
+class Question extends HTMLElement {
+    constructor () {
+        super();
+    }
+}
+
+// register custom elements
+customElements.define('quiz-next', Next);
+customElements.define('quiz-back', Back);
+customElements.define('quiz-restart', Restart);
