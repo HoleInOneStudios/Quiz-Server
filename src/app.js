98af53662869c5122259e922d0466e3ba3bf86d7
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-09-11 20:15:37 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ var express = require('express');
 var path = require('path');
 
 //Routes
-var indexRouter = require('./src/routes/index');
-var quizRouter = require('./src/routes/quiz');
+var indexRouter = require('./routes/index');
+var quizRouter = require('./routes/quiz');
 
 var app = express();
 
 //view engine setup
-app.set('views', path.join(__dirname, './src/views'))
+app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/src/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', indexRouter)
 app.use('/', quizRouter)
--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,5 +1,5 @@
 const supertest = require('supertest');
-const app = require('../../app');
+const app = require('../app');
 const requestWithSupertest = supertest(app);
 
 describe('Test the routes', () => {
