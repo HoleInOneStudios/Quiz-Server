896e77d01aecfec14c8ce83c916b10bd514e18a0
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-09-11 20:24:45 -0500

    tests

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,10 +1,18 @@
-const supertest = require('supertest');
 const app = require('../app');
+const fm = require('../bin/getData');
+
+const supertest = require('supertest');
 const requestWithSupertest = supertest(app);
 
-describe('Test the routes', () => {
+describe('Routes', () => {
     it('GET /', async () => {
         const res = await requestWithSupertest.get('/');
         expect(res.statusCode).toBe(200);
     });
+});
+
+describe('Files', () => {
+    it('GET Workbook', async () => {
+        expect(fm.WB).not.toBe(null);
+    })
 });
