a923e80da9042362cc0665447c472a8cb03f3061
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-09-30 13:20:26 -0500

    misc

--- src/public/sw.js
+++ src/public/sw.js
@@ -10,13 +10,13 @@ const filesToCache = [
 
 // Install Service Worker
 self.addEventListener('install', function (event) {
-    console.log('Service Worker: Installing....');
+    //console.log('Service Worker: Installing....');
 
     event.waitUntil(
 
         // Open the Cache
         caches.open(cacheName).then(function (cache) {
-            console.log('Service Worker: Caching App Shell at the moment......');
+            //console.log('Service Worker: Caching App Shell at the moment......');
 
             // Add Files to the Cache
             return cache.addAll(filesToCache);
@@ -27,13 +27,13 @@ self.addEventListener('install', function (event) {
 // Fired when the Service Worker starts up
 self.addEventListener('activate', function (event) {
 
-    console.log('Service Worker: Activating....');
+    //console.log('Service Worker: Activating....');
 
     event.waitUntil(
         caches.keys().then(function (cacheNames) {
             return Promise.all(cacheNames.map(function (key) {
                 if (key !== cacheName) {
-                    console.log('Service Worker: Removing Old Cache', key);
+                    //console.log('Service Worker: Removing Old Cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -44,9 +44,9 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
 
-    console.log('Service Worker: Fetch', event.request.url);
+    //console.log('Service Worker: Fetch', event.request.url);
 
-    console.log("Url", event.request.url);
+    //console.log("Url", event.request.url);
 
     event.respondWith(
         caches.match(event.request).then(function (response) {
