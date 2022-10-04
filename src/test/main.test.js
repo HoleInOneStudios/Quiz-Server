ac5c925fb007f02d74e239b2043a63d750b3c255
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-10-04 15:03:24 -0500

    tests

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -7,25 +7,37 @@ const supertest = require('supertest');
 
 const requestWithSupertest = supertest(app);
 
+beforeAll(() => {
+
+});
+
 describe('Routes', () => {
     it('GET /', async () => {
         const res = await requestWithSupertest.get('/');
         expect(res.statusCode).toBe(200);
     });
+    describe('Quiz Routes', () => {
+        fm.WB.SheetNames.forEach(sheet => {
+            it(`GET /${sheet}`, async () => {
+                const res = await requestWithSupertest.get(`/${sheet}`);
+                expect(res.statusCode).toBe(200);
+            });
+        });
+    });
 });
 
 describe('Files', () => {
-    it('GET Workbook', () => {
+    it('GET Workbook', async () => {
         expect(fm.WB).not.toBe(null);
     })
-    it('GET public directory', () => {
+    it('GET public directory', async () => {
         expect(fs.existsSync(path.join(__dirname, "../public"))).toBe(true);
     });
-    it('GET Views', () => {
+    it('GET Views', async () => {
         expect(fs.existsSync(path.join(__dirname, "../views"))).toBe(true);
     })
-
-    afterAll(() => {
-        fm.UnWatchFile();
-    })
 });
+
+afterAll(() => {
+    fm.UnWatchFile();
+})
