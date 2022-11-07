a4fe45fcaa1369d32bc3e6146849a12ce0808f51
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-11-07 15:40:12 -0600

    ts dom

--- docs/tutorials/quiz.ts
+++ docs/tutorials/quiz.ts
@@ -4,10 +4,44 @@ class Quiz {
     totalQuestion: number = 0;
     score: number = 0;
 
+
     constructor(questions: Question[]) {
         this.questions = questions;
 
         this.totalQuestion = questions.length;
+    }
+
+    nextQuestion() {
+        if (this.isFinished()) {
+            return;
+        } else {
+            this.currentQuestion++;
+        }
+    }
+
+    previousQuestion() {
+        if (this.isFirstQuestion()) {
+            return;
+        } else {
+            this.currentQuestion--;
+        }
+    }
+
+    isFinished() {
+        return this.currentQuestion >= this.totalQuestion;
+    }
+
+    isFirstQuestion() {
+        return this.currentQuestion <= 0;
+    }
+
+    reset() {
+        this.currentQuestion = 0;
+        this.score = 0;
+
+        this.questions.forEach(question => {
+            question.reset();
+        });
     }
 }
 
@@ -17,12 +51,21 @@ class Question {
     maxTries: number;
     tries: number = 0;
 
+    hint: string;
+    hintImage: string;
+    backgroundImage: string;
+
+
     correct: boolean = false;
     maxTriesExceeded: boolean = false;
 
-    constructor(text: string, answers: Answer[]) {
+    constructor(text: string, answers: Answer[], hint: string, HintImage: string, BackgroundImage: string) {
         this.text = text;
         this.answers = answers;
+
+        this.hint = hint;
+        this.hintImage = HintImage;
+        this.backgroundImage = BackgroundImage;
 
         if (this.answers.length > 2) {
             this.maxTries = 2;
@@ -30,6 +73,16 @@ class Question {
         else {
             this.maxTries = 1;
         }
+    }
+
+    reset() {
+        this.tries = 0;
+        this.correct = false;
+        this.maxTriesExceeded = false;
+
+        this.answers.forEach(answer => {
+            answer.reset();
+        });
     }
 }
 
@@ -39,5 +92,9 @@ class Answer {
     selected: boolean = false;
     constructor(text: string, correct: boolean) {
         this.text = text;
+    }
+
+    reset() {
+        this.selected = false;
     }
 }
