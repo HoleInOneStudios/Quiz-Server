eb93b3dedc0467cbb3d612a0d52fd2d58664cee9
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2023-01-10 20:15:44 -0600

    crash exploit

--- src/routes/data.js
+++ src/routes/data.js
@@ -14,14 +14,12 @@ router.post('/send_score', function (req, res) {
     //Check if session data file exists if not create one
     if (!fs.existsSync(sessionDataFile)) {
         fs.appendFile(sessionDataFile, "", function (err) {
-            if (err) throw err;
             console.log('Data File Created...');
         });
     }
 
     //Write Session to file
     fs.appendFile(sessionDataFile, JSON.stringify(req.body.SESSION) + "\n", function (err) {
-        if (err) throw err;
         console.log('Session Data Saved...');
     });
 });
