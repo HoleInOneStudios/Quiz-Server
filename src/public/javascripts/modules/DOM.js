ea26e7f64a037946cef2fb2d911a7ed51ff5a702
Ben Bowles <benjamin.d.bowles@gmail.com>
2023-04-05 15:25:49 -0500

    updates

--- src/public/javascripts/modules/DOM.js
+++ src/public/javascripts/modules/DOM.js
@@ -1,35 +1,37 @@
 // Get DOM elements
-const START = $( 'quiz-start' ).get( 0 );
-const INFO = $( 'quiz-info' ).get( 0 );
+const START = $("quiz-start").get(0);
+const INFO = $("quiz-info").get(0);
 
-const MAIN = $('quiz-main').get(0);
-const QUESTION = $('quiz-question').get(0);
-const ANSWERS = $('quiz-answer');
+const MAIN = $("quiz-main").get(0);
+const QUESTION = $("quiz-question").get(0);
+const ANSWERS = $("quiz-answer");
 
-const HINT_TOGGLE = $('quiz-hint-toggle').get(0);
-const HINT_TEXT = $('quiz-hint-text').get(0);
+const HINT_TOGGLE = $("quiz-hint-toggle").get(0);
+const HINT_TEXT = $("quiz-hint-text").get(0);
 
-const QUIZ_STATUS = $('quiz-current-question').get(0);
-const QUIZ_SCORE = $('quiz-score').get(0);
-const QUIZ_TRIES = $('quiz-tries').get(0);
+const QUIZ_STATUS = $("quiz-current-question").get(0);
+const QUIZ_SCORE = $("quiz-score").get(0);
+const QUIZ_TRIES = $("quiz-tries").get(0);
 
-const AUDIO_TOGGLE = $('quiz-audio-toggle').get(0);
+const AUDIO_TOGGLE = $("quiz-audio-toggle").get(0);
+console.log(AUDIO_TOGGLE);
 
-const FINISH = $('quiz-finish').get(0);
+const FINISH = $("quiz-finish").get(0);
 
-const QUIZ_RESULTS = $('quiz-results').get(0);
+const QUIZ_RESULTS = $("quiz-results").get(0);
 
-const LOGO = $('quiz-logo').get(0);
+const LOGO = $("quiz-logo").get(0);
 
 // Audio DOM
-const CORRECT = $('#correct_Audio').get(0);
-const INCORRECT = $( '#incorrect_Audio' ).get( 0 );
-const MUSIC = $( '#music_Audio' ).get( 0 );
+const CORRECT = $("#correct_Audio").get(0);
+const INCORRECT = $("#incorrect_Audio").get(0);
+const MUSIC = $("#music_Audio").get(0);
 
-const AUDIOS = $( 'audio' );
+const AUDIOS = $("audio");
+console.log(AUDIOS);
 
 // nav buttons
-const NEXT_QUESTION = $('quiz-next').get(0);
-const RESTART = $('quiz-restart').get(0);
+const NEXT_QUESTION = $("quiz-next").get(0);
+const RESTART = $("quiz-restart").get(0);
 
 console.log("DOM.js Loaded Successfully ✅...");
--- src/public/javascripts/quiz.js
+++ src/public/javascripts/quiz.js
@@ -1,8 +1,16 @@
 // Set background images
-START.style.backgroundImage = SHEET_DATA[ 0 ].bgImage ? `url('./img/${ SHEET_DATA[ 0 ].bgImage }')` : `url('./public/images/bg-placeholder.jpg')`;
-INFO.style.backgroundImage = SHEET_DATA[ 0 ].bgImage ? `url('./img/${ SHEET_DATA[ 0 ].bgImage }')` : `url('./public/images/bg-placeholder.jpg')`;
-MAIN.style.backgroundImage = SHEET_DATA[ 0 ].bgImage ? `url('./img/${ SHEET_DATA[ 0 ].bgImage }')` : `url('./public/images/bg-placeholder.jpg')`;
-FINISH.style.backgroundImage = SHEET_DATA[ 0 ].bgImage ? `url('./img/${ SHEET_DATA[ 0 ].bgImage }')` : `url('./public/images/bg-placeholder.jpg')`;
+START.style.backgroundImage = SHEET_DATA[0].bgImage
+  ? `url('./img/${SHEET_DATA[0].bgImage}')`
+  : `url('./public/images/bg-placeholder.jpg')`;
+INFO.style.backgroundImage = SHEET_DATA[0].bgImage
+  ? `url('./img/${SHEET_DATA[0].bgImage}')`
+  : `url('./public/images/bg-placeholder.jpg')`;
+MAIN.style.backgroundImage = SHEET_DATA[0].bgImage
+  ? `url('./img/${SHEET_DATA[0].bgImage}')`
+  : `url('./public/images/bg-placeholder.jpg')`;
+FINISH.style.backgroundImage = SHEET_DATA[0].bgImage
+  ? `url('./img/${SHEET_DATA[0].bgImage}')`
+  : `url('./public/images/bg-placeholder.jpg')`;
 
 // Set logo
 LOGO.innerHTML = `<img src="./img/logo.png" alt="Logo">`;
@@ -12,298 +20,274 @@ let CURRENT_STATE = QUIZ_STATE.START;
 updateDOMState();
 
 /** Set Hint Image  */
-function setHint ()
-{
-    //HINT_TOGGLE.style.backgroundImage = SHEET_DATA[CURRENT_QUESTION].hImage ? SHEET_DATA[CURRENT_QUESTION].hImage : "./public/images/Hint-Person-Placeholder.png";
-    HINT_TOGGLE.innerHTML = SHEET_DATA[ CURRENT_QUESTION ].hImage ? `<img src="./img/${ SHEET_DATA[ 0 ].hImage }" alt="Hint Image">` : `<img src="./public/images/Hint-Person-Placeholder.png" alt="Hint Image">`;
-    HINT_TEXT.innerHTML = SHEET_DATA[ CURRENT_QUESTION ].hint;
+function setHint() {
+  //HINT_TOGGLE.style.backgroundImage = SHEET_DATA[CURRENT_QUESTION].hImage ? SHEET_DATA[CURRENT_QUESTION].hImage : "./public/images/Hint-Person-Placeholder.png";
+  HINT_TOGGLE.innerHTML = SHEET_DATA[CURRENT_QUESTION].hImage
+    ? `<img src="./img/${SHEET_DATA[0].hImage}" alt="Hint Image">`
+    : `<img src="./public/images/Hint-Person-Placeholder.png" alt="Hint Image">`;
+  HINT_TEXT.innerHTML = SHEET_DATA[CURRENT_QUESTION].hint;
 }
 /** Toggle Hint and set `hint_used` to `true` for the session */
-function toggleHint ()
-{
-    SESSION.QUESTIONS[ CURRENT_QUESTION ].hint_used = true;
-    HINT_TEXT.classList.toggle( 'hidden' );
+function toggleHint() {
+  SESSION.QUESTIONS[CURRENT_QUESTION].hint_used = true;
+  HINT_TEXT.classList.toggle("hidden");
 }
 /** Hide the Hint */
-function disableHint ()
-{
-    HINT_TEXT.classList.toggle( 'hidden', true );
+function disableHint() {
+  HINT_TEXT.classList.toggle("hidden", true);
 }
 
 /** update DOM based on state */
-function updateDOMState ()
-{
-    switch ( CURRENT_STATE )
-    {
-        case QUIZ_STATE.START:
-            START.style.display = 'flex';
-            INFO.style.display = 'none';
-            MAIN.style.display = 'none';
-            FINISH.style.display = 'none';
-            break;
-        case QUIZ_STATE.INFO:
-            START.style.display = 'none';
-            INFO.style.display = 'flex';
-            MAIN.style.display = 'none';
-            FINISH.style.display = 'none';
-            break;
-        case QUIZ_STATE.MAIN:
-            START.style.display = 'none';
-            INFO.style.display = 'none';
-            MAIN.style.display = 'flex';
-            FINISH.style.display = 'none';
-            break;
-        case QUIZ_STATE.FINISH:
-            START.style.display = 'none';
-            INFO.style.display = 'none';
-            MAIN.style.display = 'none';
-            FINISH.style.display = 'flex';
-            break;
-    }
+function updateDOMState() {
+  switch (CURRENT_STATE) {
+    case QUIZ_STATE.START:
+      START.style.display = "flex";
+      INFO.style.display = "none";
+      MAIN.style.display = "none";
+      FINISH.style.display = "none";
+      break;
+    case QUIZ_STATE.INFO:
+      START.style.display = "none";
+      INFO.style.display = "flex";
+      MAIN.style.display = "none";
+      FINISH.style.display = "none";
+      break;
+    case QUIZ_STATE.MAIN:
+      START.style.display = "none";
+      INFO.style.display = "none";
+      MAIN.style.display = "flex";
+      FINISH.style.display = "none";
+      break;
+    case QUIZ_STATE.FINISH:
+      START.style.display = "none";
+      INFO.style.display = "none";
+      MAIN.style.display = "none";
+      FINISH.style.display = "flex";
+      break;
+  }
 }
 
 /** Set state function - Start */
-function setStateStart ()
-{
-    CURRENT_STATE = QUIZ_STATE.START;
-    CURRENT_QUESTION = 0;
+function setStateStart() {
+  CURRENT_STATE = QUIZ_STATE.START;
+  CURRENT_QUESTION = 0;
 
-    resetSession();
-    updateDOMState();
+  resetSession();
+  updateDOMState();
 }
 
-function setStateInfo ()
-{
-    CURRENT_QUESTION = 0;
+function setStateInfo() {
+  CURRENT_QUESTION = 0;
 
-    resetSession();
+  resetSession();
 
-    CURRENT_STATE = QUIZ_STATE.INFO;
-    updateDOMState();
+  CURRENT_STATE = QUIZ_STATE.INFO;
+  updateDOMState();
 }
 
 /** Set state function - Main */
-function setStateMain ()
-{
-    CURRENT_STATE = QUIZ_STATE.MAIN;
-    resetSession();
+function setStateMain() {
+  CURRENT_STATE = QUIZ_STATE.MAIN;
+  resetSession();
 
-    disableHint();
-    setHint();
+  disableHint();
+  setHint();
 
-    MUSIC.play();
+  MUSIC.play();
 
-    loadQuestion();
-    updateStatus();
+  loadQuestion();
+  updateStatus();
 
-    SESSION.START_TIME = Date.now();
-    updateDOMState();
+  SESSION.START_TIME = Date.now();
+  updateDOMState();
 }
 
 /** Set state function - Finish  */
-function setStateFinish ()
-{
-    CURRENT_STATE = QUIZ_STATE.FINISH;
-    SESSION.END_TIME = Date.now();
+function setStateFinish() {
+  CURRENT_STATE = QUIZ_STATE.FINISH;
+  SESSION.END_TIME = Date.now();
 
-    finishDOM();
-    updateDOMState();
-    sendData();
+  finishDOM();
+  updateDOMState();
+  sendData();
 }
 
 /** Reset SESSION */
-function resetSession ()
-{
-    SESSION.SCORE = 0;
-    SESSION.START_TIME = 0;
-    SESSION.END_TIME = 0;
-    for ( var i = 0; i < SESSION.QUESTIONS.length; i++ )
-    {
-        SESSION.QUESTIONS[ i ].selections = [];
-        SESSION.QUESTIONS[ i ].correct = false;
-        SESSION.QUESTIONS[ i ].hint_used = false;
-        SESSION.QUESTIONS[ i ].tries_taken = 0;
-    }
+function resetSession() {
+  SESSION.SCORE = 0;
+  SESSION.START_TIME = 0;
+  SESSION.END_TIME = 0;
+  for (var i = 0; i < SESSION.QUESTIONS.length; i++) {
+    SESSION.QUESTIONS[i].selections = [];
+    SESSION.QUESTIONS[i].correct = false;
+    SESSION.QUESTIONS[i].hint_used = false;
+    SESSION.QUESTIONS[i].tries_taken = 0;
+  }
 }
 
 /** Next Question */
-function nextQuestion ()
-{
-    if ( CURRENT_QUESTION < SHEET_DATA.length - 1 )
-    {
-        CURRENT_QUESTION++;
-    } else
-    {
-        setStateFinish();
-    }
+function nextQuestion() {
+  if (CURRENT_QUESTION < SHEET_DATA.length - 1) {
+    CURRENT_QUESTION++;
+  } else {
+    setStateFinish();
+  }
 
-    loadQuestion();
-    disableHint();
-    setHint();
-    updateStatus();
+  loadQuestion();
+  disableHint();
+  setHint();
+  updateStatus();
 }
 
 /** finish DOM */
-function finishDOM ()
-{
-    QUIZ_RESULTS.innerHTML =
-        `<h2>${ SESSION.SCORE / SHEET_DATA.length * 100 > 70 ? 'Congratulations!' : 'Better Luck Next Time!' }</h2>
+function finishDOM() {
+  QUIZ_RESULTS.innerHTML = `<h2>${
+    (SESSION.SCORE / SHEET_DATA.length) * 100 > 70
+      ? "Congratulations!"
+      : "Better Luck Next Time!"
+  }</h2>
          <p>You have completed the quiz!</p>
-         <p>Your score is: ${ parseInt( SESSION.SCORE / SHEET_DATA.length * 100 ) }%</p>`;
+         <p>Your score is: ${parseInt(
+           (SESSION.SCORE / SHEET_DATA.length) * 100
+         )}%</p>`;
 }
 
 /** Status */
-function updateStatus ()
-{
-    QUIZ_STATUS.innerHTML = `Question ${ CURRENT_QUESTION + 1 }/${ SHEET_DATA.length }`;
-    QUIZ_SCORE.innerHTML = `Score ${ parseInt( SESSION.SCORE / SHEET_DATA.length * 100 ) }%`;
-    QUIZ_TRIES.innerHTML = `${ TRIES }/${ MAX_TRIES } Tries Remaining`;
+function updateStatus() {
+  QUIZ_STATUS.innerHTML = `Question ${CURRENT_QUESTION + 1}/${
+    SHEET_DATA.length
+  }`;
+  QUIZ_SCORE.innerHTML = `Score ${parseInt(
+    (SESSION.SCORE / SHEET_DATA.length) * 100
+  )}%`;
+  QUIZ_TRIES.innerHTML = `${TRIES}/${MAX_TRIES} Tries Remaining`;
 }
 
 /** load question */
-function loadQuestion ()
-{
-    // Set the question
-    QUESTION.innerHTML = SHEET_DATA[ CURRENT_QUESTION ].question;
+function loadQuestion() {
+  // Set the question
+  QUESTION.innerHTML = SHEET_DATA[CURRENT_QUESTION].question;
 
-    // Set the answers
-    for ( var i = 0; i < ANSWERS.length; i++ )
-    {
-        if ( SHEET_DATA[ CURRENT_QUESTION ].answers[ i ] )
-        {
-            ANSWERS[ i ].classList.toggle( 'correct', false );
-            ANSWERS[ i ].classList.toggle( 'incorrect', false );
-            ANSWERS[ i ].innerHTML = SHEET_DATA[ CURRENT_QUESTION ].answers[ i ];
-            ANSWERS[ i ].classList.toggle( 'hidden', false );
-        } else
-        {
-            ANSWERS[ i ].classList.toggle( 'hidden', true );
-        }
+  // Set the answers
+  for (var i = 0; i < ANSWERS.length; i++) {
+    if (SHEET_DATA[CURRENT_QUESTION].answers[i]) {
+      ANSWERS[i].classList.toggle("correct", false);
+      ANSWERS[i].classList.toggle("incorrect", false);
+      ANSWERS[i].innerHTML = SHEET_DATA[CURRENT_QUESTION].answers[i];
+      ANSWERS[i].classList.toggle("hidden", false);
+    } else {
+      ANSWERS[i].classList.toggle("hidden", true);
     }
+  }
 
-    // remove animation from nextQuestion
-    NEXT_QUESTION.classList.remove( 'animate-button' );
+  // remove animation from nextQuestion
+  NEXT_QUESTION.classList.remove("animate-button");
 
-    // set Tries
-    MAX_TRIES = 0;
-    for ( var i = 0; i < SHEET_DATA[ CURRENT_QUESTION ].answers.length; i++ )
-    {
-        if ( SHEET_DATA[ CURRENT_QUESTION ].answers[ i ] )
-        {
-            MAX_TRIES++;
-        }
+  // set Tries
+  MAX_TRIES = 0;
+  for (var i = 0; i < SHEET_DATA[CURRENT_QUESTION].answers.length; i++) {
+    if (SHEET_DATA[CURRENT_QUESTION].answers[i]) {
+      MAX_TRIES++;
     }
-    MAX_TRIES--;
-    TRIES = MAX_TRIES;
+  }
+  MAX_TRIES--;
+  TRIES = MAX_TRIES;
 }
 
 /** answer */
-function answerEvent ( answerIndex )
-{
-    if ( TRIES > 0 && !SESSION.QUESTIONS[ CURRENT_QUESTION ].selections.includes( answerIndex ) && !SESSION.QUESTIONS[ CURRENT_QUESTION ].correct )
-    {
-        if ( SHEET_DATA[ CURRENT_QUESTION ].correctAnswers[ answerIndex ] )
-        {
-            CORRECT.currentTime = 0;
-            CORRECT.play();
-            SESSION.QUESTIONS[ CURRENT_QUESTION ].correct = true;
-            SESSION.SCORE++;
-            ANSWERS[ answerIndex ].classList.toggle( 'correct', true );
-            NEXT_QUESTION.classList.toggle( 'animate-button', true );
-        }
-        else
-        {
-            INCORRECT.currentTime = 0;
-            INCORRECT.play();
-            ANSWERS[ answerIndex ].classList.toggle( 'incorrect', true );
-        }
-        SESSION.QUESTIONS[ CURRENT_QUESTION ].selections.push( answerIndex );
-        TRIES--;
-        SESSION.QUESTIONS[ CURRENT_QUESTION ].tries_taken++;
-
-        if ( TRIES <= 0 )
-        {
-            // reveal correct answer
-            for ( var i = 0; i < SHEET_DATA[ CURRENT_QUESTION ].correctAnswers.length; i++ )
-            {
-                if ( SHEET_DATA[ CURRENT_QUESTION ].correctAnswers[ i ] )
-                {
-                    ANSWERS[ i ].classList.toggle( 'correct', true );
-                }
-            }
-            NEXT_QUESTION.classList.toggle( 'animate-button', true );
-        }
-
-        updateStatus();
+function answerEvent(answerIndex) {
+  if (
+    TRIES > 0 &&
+    !SESSION.QUESTIONS[CURRENT_QUESTION].selections.includes(answerIndex) &&
+    !SESSION.QUESTIONS[CURRENT_QUESTION].correct
+  ) {
+    if (SHEET_DATA[CURRENT_QUESTION].correctAnswers[answerIndex]) {
+      CORRECT.currentTime = 0;
+      CORRECT.play();
+      SESSION.QUESTIONS[CURRENT_QUESTION].correct = true;
+      SESSION.SCORE++;
+      ANSWERS[answerIndex].classList.toggle("correct", true);
+      NEXT_QUESTION.classList.toggle("animate-button", true);
+    } else {
+      INCORRECT.currentTime = 0;
+      INCORRECT.play();
+      ANSWERS[answerIndex].classList.toggle("incorrect", true);
     }
+    SESSION.QUESTIONS[CURRENT_QUESTION].selections.push(answerIndex);
+    TRIES--;
+    SESSION.QUESTIONS[CURRENT_QUESTION].tries_taken++;
+
+    if (TRIES <= 0) {
+      // reveal correct answer
+      for (
+        var i = 0;
+        i < SHEET_DATA[CURRENT_QUESTION].correctAnswers.length;
+        i++
+      ) {
+        if (SHEET_DATA[CURRENT_QUESTION].correctAnswers[i]) {
+          ANSWERS[i].classList.toggle("correct", true);
+        }
+      }
+      NEXT_QUESTION.classList.toggle("animate-button", true);
+    }
+
+    updateStatus();
+  }
 }
 
 /** audio controls */
-function toggleAudio ()
-{
-    AUDIO = !AUDIO;
-    for ( let index = 0; index < AUDIOS.length; index++ )
-    {
-        const element = AUDIOS[ index ];
-        element.setAttribute( 'muted', !AUDIO );
-    }
-    AUDIO_TOGGLE.innerText = AUDIO ? 'volume_up' : 'volume_off';
+function toggleAudio() {
+  AUDIO = !AUDIO;
+  for (let index = 0; index < AUDIOS.length; index++) {
+    const element = AUDIOS[index];
+    element.muted = !AUDIO;
+  }
+  AUDIO_TOGGLE.innerText = AUDIO ? "volume_up" : "volume_off";
 }
 
 /** User timeout after 60 seconds of inactivity */
-function debounce ( callback, timeout, _this )
-{
-    var timer;
-    return function ( e )
-    {
-        var _that = this;
-        if ( timer )
-            clearTimeout( timer );
-        timer = setTimeout( function ()
-        {
-            callback.call( _this || _that, e );
-        }, timeout );
-    };
+function debounce(callback, timeout, _this) {
+  var timer;
+  return function (e) {
+    var _that = this;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      callback.call(_this || _that, e);
+    }, timeout);
+  };
 }
 
 /** User timeout after 60 seconds of inactivity */
-var userAction = debounce( function ( e )
-{
-    setStateStart();
-    MUSIC.pause();
-    MUSIC.currentTime = 0;
+var userAction = debounce(function (e) {
+  setStateStart();
+  MUSIC.pause();
+  MUSIC.currentTime = 0;
+}, 60 * 1000);
 
-}, 60 * 1000 );
-
-var refresh = debounce( function ( e )
-{
-    location.reload();
-}, 15 * 60 * 1000 );
+var refresh = debounce(function (e) {
+  location.reload();
+}, 15 * 60 * 1000);
 
 /** User timeout after 60 seconds of inactivity */
-document.body.onload = () =>
-{
-    document.addEventListener( "mousemove", userAction, false );
-    document.addEventListener( "click", userAction, false );
-    document.addEventListener( "scroll", userAction, false );
-    document.addEventListener( "keypress", userAction, false );
+document.body.onload = () => {
+  document.addEventListener("mousemove", userAction, false);
+  document.addEventListener("click", userAction, false);
+  document.addEventListener("scroll", userAction, false);
+  document.addEventListener("keypress", userAction, false);
 
-    document.addEventListener( "mousemove", refresh, false );
-    document.addEventListener( "click", refresh, false );
-    document.addEventListener( "scroll", refresh, false );
-    document.addEventListener( "keypress", refresh, false );
+  document.addEventListener("mousemove", refresh, false);
+  document.addEventListener("click", refresh, false);
+  document.addEventListener("scroll", refresh, false);
+  document.addEventListener("keypress", refresh, false);
 };
 
 /** send analytics to server */
-function sendData ()
-{
-    fetch( "/send_score",
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify( { SESSION } )
-        } )
-        .catch( err => console.error( err ) );
+function sendData() {
+  fetch("/send_score", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ SESSION }),
+  }).catch((err) => console.error(err));
 }
